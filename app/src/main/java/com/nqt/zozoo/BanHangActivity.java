8110926e1b1dbc8822659ce5e3863a82f1e93310
wetwetty410 <truonguyen41096@gmail.com>
2018-11-30 16:30:14 +0700

    Frist

---
+++ app/src/main/java/com/nqt/zozoo/BanHangActivity.java
@@ -0,0 +1,22 @@
+package com.nqt.zozoo;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BanHangActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ban_hang);
+
+        tabLayout=findViewById(R.id.ban_hang_tab);
+        viewPager=findViewById(R.id.ban_hang_viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/nqt/zozoo/BanHangPagerAdapter.java
@@ -0,0 +1,26 @@
+package com.nqt.zozoo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by USER on 11/30/2018.
+ */
+
+public class BanHangPagerAdapter extends FragmentPagerAdapter {
+
+    public BanHangPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
