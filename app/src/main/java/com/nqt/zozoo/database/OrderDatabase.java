27433025626c56a707df4f612d66a5a80be53122
wetwetty410 <truonguyen41096@gmail.com>
2018-12-19 16:58:07 +0700

    Frist

--- app/src/main/java/com/nqt/zozoo/database/OrderDatabase.java
+++ app/src/main/java/com/nqt/zozoo/database/OrderDatabase.java
@@ -7,8 +7,16 @@ import android.content.Context;
  */
 
 public class OrderDatabase extends DatabaseManager {
+    public static final String TABLE_ORDER = "order_mon_an";
+    public static final String TABLE_ORDER_ID = "id";
+    public static final String TABLE_ORDER_MA = "ma_order";
+    public static final String TABLE_ORDER_DANH_SACH = "danh_sach_order";
+    public static final String TABLE_ORDER_MA_BAN = "ma_ban";
+    public static final String TABLE_ORDER_THOI_GIAN = "time";
 
     public OrderDatabase(Context context) {
         super(context);
     }
+
+
 }
